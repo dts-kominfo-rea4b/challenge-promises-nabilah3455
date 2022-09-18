b4788f2c970aeaab6557e7ac6f667e4e2704f378
Gnabilah <ghoniyyatul.nabilah@ib-synergy.co.id>
2022-09-18 13:55:38 +0700

    create promiseOutput funcion

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (ekspresi) => {
+  const data1 = await promiseTheaterIXX();
+  const data2 = await promiseTheaterVGC();
+
+  const resultData = data1.concat(data2)
+
+  return resultData.filter(el => el.hasil === ekspresi).length
+};
 
 module.exports = {
   promiseOutput,
